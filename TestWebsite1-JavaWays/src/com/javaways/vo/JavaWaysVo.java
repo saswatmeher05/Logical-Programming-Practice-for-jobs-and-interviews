acb6479d143b7865b32c5d6a87f9be5c976a4b7e
saswa <saswa@DESKTOP-0F3M21B>
2020-12-10 10:40:11 +0530

    added JavaWaysVo

---
+++ TestWebsite1-JavaWays/src/com/javaways/vo/JavaWaysVo.java
@@ -0,0 +1,54 @@
+package com.javaways.vo;
+
+public class JavaWaysVo {
+	String cemail;
+	String cpassword;
+	String cphone;
+	String cgender;
+	String cdob;
+	String ccountry;
+	String cname;
+	
+	public String getCemail() {
+		return cemail;
+	}
+	public void setCemail(String cemail) {
+		this.cemail = cemail;
+	}
+	public String getCpassword() {
+		return cpassword;
+	}
+	public void setCpassword(String cpassword) {
+		this.cpassword = cpassword;
+	}
+	public String getCphone() {
+		return cphone;
+	}
+	public void setCphone(String cphone) {
+		this.cphone = cphone;
+	}
+	public String getCgender() {
+		return cgender;
+	}
+	public void setCgender(String cgender) {
+		this.cgender = cgender;
+	}
+	public String getCdob() {
+		return cdob;
+	}
+	public void setCdob(String cdob) {
+		this.cdob = cdob;
+	}
+	public String getCcountry() {
+		return ccountry;
+	}
+	public void setCcountry(String ccountry) {
+		this.ccountry = ccountry;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+}
